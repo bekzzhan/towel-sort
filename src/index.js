5c3c1b18faf09fc22945bbb7593cc786ca8dd2d6
Bekzhan Zhamantayev <bekzzhan31@gmail.com>
2021-03-09 18:08:03 +0600

    feat: implement towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-
+/* jshint esversion: 6 */
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (!matrix) {
+    return [];
+  }
+  result = [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (Array.isArray(matrix[i])) {
+      if (i % 2 === 0) {
+        for (const item of matrix[i]) {
+            result.push(item);
+        }
+      } else {
+        const arr = matrix[i].reverse();
+        for (const item of arr) {
+          result.push(item);
+        }
+      }
+    } else {
+        result.push(matrix[i]);
+    }
+  }
+  return result;
+};
